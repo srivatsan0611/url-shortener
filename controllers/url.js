892143472e2016e2ae636b5ad7d36a3e299d007a
srivatsan0611 <srivatsansrinivasan03@gmail.com>
2024-08-19 14:14:51 +0530

    Create NanoID generator

---
+++ controllers/url.js
@@ -0,0 +1,20 @@
+import { nanoid } from "nanoid";
+import URL from "../models/url.js";
+
+generateNewShortURL = async (req, res) => {
+
+    const body = req.body;
+    if (!body) {
+        return res.status(400).json({success: true, message: "Bad Request"});
+    }
+    const shortID = nanoid(6);
+    await URL.create({
+        shortId: shortID,
+        redirectURL: body.url,
+        visitHistory: []
+    });
+
+    return res.status(200).json({shortId: shortID});
+}
+
+export default generateNewShortURL;
