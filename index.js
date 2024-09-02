74e58325ac388a3cd0c3f048f68ede661765eca2
srivatsan0611 <srivatsansrinivasan03@gmail.com>
2024-09-02 16:23:36 +0530

    Include nanoId and push visitHistory

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import router from "./routes/url.js";
 import connectToMongoDB from "./connect.js";
+import URL from "./models/url.js";
 
 const app = express();
 const PORT = 5000;
@@ -11,8 +12,23 @@ app.use(express.json());
 
 app.use('/url', router);
 
+app.get('/:shortId', async (req, res) => {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({ shortId }
+        , {
+            $push: {
+                visitHistory: {
+                    timestamp: Date.now()
+                }
+            }
+        }
+    );
+    res.redirect(entry.redirectURL);
+
+});
+
 app.listen(PORT, () => {
     connectToMongoDB();
-    console.log(`Server started at https://localhost:${PORT}`);
+    console.log(`Server started at http://localhost:${PORT}`);
 });
 
